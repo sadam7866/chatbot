98239c5f01ab7c618fb8aca0bfcbb7948e6374b6
Sadam <assadam786@gmail.com>
2019-07-28 14:45:10 +0530

    chat setup

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+$method = $_SERVER['REQUEST_METHOD'];
+//$method ='GET';
+//$LanID ='sguna002';
+if ($method == 'POST')
+{
+    $requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+	$LanID = $json->result->parameters->LanID;
+	$username = 'Administrator';
+	$password = 'manage';
+	//$URL = "http://192.168.0.9:8055/invoke/Default:new_flowservice?num1=2&num2=4";
+	//$URL = "http://localhost:8055/invoke/Default:new_flowservice?num1=2&num2=4";
+	//$URL = 'https://localhost:8999/rest/Default/new_rest/_get?num1=2&num2=4';
+	//https://localhost:8999/rest/Default/new_rest/_get?num1=1&num2=4
+    
+     switch ($LanID) {
+    
+        case "shuss098":
+		  
+		       $Status_MSG = "Your Cognizant ID $LanID has been validated. Thanks";
+		     $URL="http://192.168.43.27:5555/rest/ChatBot/myRest?num1=2&num2=4";
+$opts = array('http' =>
+ array(
+       'header'    => ['Content-type: application/json' , 'Accept: application/json', 'Authorization: Basic '.base64_encode("$username:$password")], 'method'    => 'GET'));
+$context = stream_context_create($opts);
+$jsonStr = file_get_contents($URL, false, $context);
+$obj = json_decode($jsonStr);
+		    // $Status_MSG = $jsonStr;
+$Status_MSG = $obj->{'sumTotal'};
+// $Status_MSG = "Your Cognizant ID $LanID/boss has been validated. Thanks";
+		    	  break;
+            
+        default:
+            $Status_MSG = "Your Cognizant  ID is not valid. Thanks";
+            break;
+    }
+	
+    $response = new \stdClass();
+    $response->speech = "ulaganayagan" . $Status_MSG;
+    $response->displayText = "ulaganayagan" . $Status_MSG;
+    $response->source = "webhook";
+    echo json_encode($response);
+}
+else
+{
+    //echo "This method not allowed here";
+    $response = new \stdClass();
+    $response->speech = "This method not allowed here";
+    $response->displayText = "This method not allowed here";
+    $response->source = "webhook";
+    echo json_encode($response);
+}
+?>
